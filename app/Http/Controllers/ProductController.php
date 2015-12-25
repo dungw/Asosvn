f5f64869ad4a03ba1bc4a6aeeee1ded46dc5ac81
Nguyễn Như Thép <thepnn@appnets.org>
2015-12-25 09:11:51 +0700

    product detail

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -7,9 +7,30 @@ use Illuminate\Http\Request;
 
 class ProductController extends BaseController
 {
+    /**
+     * list all product
+     * @return string
+     */
     public function index()
     {
-        $list = array('name' => 'thep');
+        $list = Product::all();
+
         return json_encode($list);
+    }
+
+    /**
+     * get product details
+     * @param $id
+     * @return array|string
+     */
+    public function show($id)
+    {
+        $product = Product::find($id);
+
+        if ($product) {
+            return json_encode($product);
+        }
+
+        return [];
     }
 }
