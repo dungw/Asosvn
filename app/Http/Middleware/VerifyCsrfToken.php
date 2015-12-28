1b05d14b415bfcb0e6abee6905afae9436f32dcc
Dzung Vuong <dung.vuong@niteco.se>
2015-12-28 10:38:52 +0700

    Add slug to product.

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -3,18 +3,38 @@
 use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
-class VerifyCsrfToken extends BaseVerifier {
+class VerifyCsrfToken extends BaseVerifier
+{
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
 	public function handle($request, Closure $next)
 	{
+		if ($this->excludedRoutes($request))
+		{
+			return $this->addCookieToResponse($request, $next($request));
+		}
+
 		return parent::handle($request, $next);
+	}
+
+	/**
+	 * Function check if request is an API, then pass the CSRF verification
+	 *
+	 * @param $request
+	 * @return bool
+	 */
+	protected function excludedRoutes($request)
+	{
+		$listAPI = ['api/product/store'];
+
+		foreach ($listAPI as $api)
+		{
+			if ($request->is($api))
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 }
