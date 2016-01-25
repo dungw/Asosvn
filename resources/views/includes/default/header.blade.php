4faaae060e593f58becbc0e9b8b253414d7f347a
Nguyễn Như Thép <thepnn@appnets.org>
2016-01-25 15:39:40 +0700

    email info

--- resources/views/includes/default/header.blade.php
+++ resources/views/includes/default/header.blade.php
@@ -5,7 +5,7 @@
                 <div class="contactinfo">
                     <ul class="nav nav-pills">
                         <li><a href="#"><i class="fa fa-phone"></i> +2 95 01 88 821</a></li>
-                        <li><a href="#"><i class="fa fa-envelope"></i> info@domain.com</a></li>
+                        <li><a href="mailto:salezone.vn@gmail.com"><i class="fa fa-envelope"></i> salezone.vn@gmail.com</a></li>
                     </ul>
                 </div>
             </div>
