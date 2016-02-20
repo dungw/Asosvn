50b4b5da352c2d1373bc6c8ff9f83f7cea96c9af
nhuthep <nhuthep.romeo@gmail.com>
2016-02-20 10:59:43 +0700

    add map to contact.blade.php

--- resources/views/cms/contact.blade.php
+++ resources/views/cms/contact.blade.php
@@ -4,6 +4,21 @@
     {{ trans('lang.Contact') }}
 @stop
 
+@section('head')
+<script src="http://maps.googleapis.com/maps/api/js"></script>
+<script>
+    function initialize() {
+      var mapProp = {
+        center:new google.maps.LatLng(21.0076406,105.791612),
+        zoom:14,
+        mapTypeId:google.maps.MapTypeId.ROADMAP
+      };
+      var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+@stop
+
 @section('content')
     <section>
         <div class="container">
@@ -13,7 +28,7 @@
                 </div>
 
                 <div class="col-sm-9">
-                    <div class="col-sm-12">
+                    <div class="col-sm-12 col-md-12">
                         @if (count($errors) > 0)
                             <div class="alert alert-danger center-block">
                                 <ul>
@@ -24,26 +39,37 @@
                             </div>
                         @endif
                     </div>
+               
+                    <div class="col-sm-4 col-md-4">
+                        {!! Form::open(['method' => 'post', 'url' => 'contact/create']) !!}
+                            <div class="form-group">
+                                <label for="name">{{ trans('lang.Name') }}</label>
+                                <input type="text" name="name" class="form-control" id="name" @if (Auth::user())
+                                       value="{{ Auth::user()->name }}" @endif placeholder="{{ trans('lang.Name') }}" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">{{ trans('lang.Email') }}</label>
+                                <input type="email" name="email" class="form-control" id="email" @if (Auth::user())
+                                       value="{{ Auth::user()->email }}" @endif placeholder="{{ trans('lang.Email') }}"
+                                       required>
+                            </div>
+                            <div class="form-group">
+                                <label for="content">{{ trans('lang.Content') }}</label>
+                                <textarea name="content" class="form-control" id="content" rows="6"
+                                          placeholder="{{ trans('lang.Your comment') }}..." required></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary center-block">{{ trans('lang.Submit') }}</button>
+                        {!! Form::close() !!}
+                    </div>
 
-                    {!! Form::open(['method' => 'post', 'url' => 'contact/create']) !!}
-                    <div class="form-group">
-                        <label for="name">{{ trans('lang.Name') }}</label>
-                        <input type="text" name="name" class="form-control" id="name" @if (Auth::user())
-                               value="{{ Auth::user()->name }}" @endif placeholder="{{ trans('lang.Name') }}" required>
+                    <div class="col-sm-4 col-md-4">                        
+                        <p>Mr.Dung: 012345789</p>
+                        <p>Mr.Thep: 012345789</p>
+                        <p>Email: <a href="mailto:salezone.vn@gmail.com">salezone.vn@gmail.com</a></p>
+                        <address>No.9999 Tran Duy Hung, Ha Noi</address>
                     </div>
-                    <div class="form-group">
-                        <label for="email">{{ trans('lang.Email') }}</label>
-                        <input type="email" name="email" class="form-control" id="email" @if (Auth::user())
-                               value="{{ Auth::user()->email }}" @endif placeholder="{{ trans('lang.Email') }}"
-                               required>
-                    </div>
-                    <div class="form-group">
-                        <label for="content">{{ trans('lang.Content') }}</label>
-                        <textarea name="content" class="form-control" id="content" rows="6"
-                                  placeholder="{{ trans('lang.Your comment') }}..." required></textarea>
-                    </div>
-                    <button type="submit" class="btn btn-primary center-block">{{ trans('lang.Submit') }}</button>
-                    {!! Form::close() !!}
+
+                    <div id="googleMap" class="col-sm-4 col-md-4" style="width:33.3%; height:300px"></div>
                 </div>
             </div>
         </div>
